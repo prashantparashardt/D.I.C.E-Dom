2bd2feb6e381527c8aeacb3ae106ffbc6224ae8f
prashantparashardt <67552769+prashantparashardt@users.noreply.github.com>
2020-07-08 00:08:19 +0530

    Image Fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;
 var randomNumber2=Math.floor(Math.random()*6)+1;
-document.querySelector("div img").setAttribute("src","images/Dice"+randomNumber1+".png");
+document.querySelector("div img").setAttribute("src","images/dice"+randomNumber1+".png");
 
-document.querySelectorAll("div img")[1].setAttribute("src","images/Dice"+randomNumber2+".png");
+document.querySelectorAll("div img")[1].setAttribute("src","images/dice"+randomNumber2+".png");
 
 if(randomNumber1>randomNumber2){
     document.querySelector("h1").innerHTML="&#128525 Player1 wins";
